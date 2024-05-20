2d9f3fb8f6046b89807d0a6c3e7de7cfdf091565
mars_salada12 <gonzalezbeltranmarcela@gmail.com>
2024-05-20 12:54:59 -0600

    header

--- templates/header.php
+++ templates/header.php
@@ -5,13 +5,10 @@
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Catalago de producto</title>
+	<title>Lista de alumnos</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 	<div class="center-align"> 
-		<h1>Catalago de producto</h1>
-		<p>Cristopher Dered Urbina Alonso - 19051223</p>
-		<p>Computo en la nube</p>
-		<br>
+		<h1>Lista de alumnos</h1>
 	</div>
